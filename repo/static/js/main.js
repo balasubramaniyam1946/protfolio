af91657cd2cc89543d8e2e89ffd386fcb3b86572
Balasubramaniyam S <balasubaramaniyam002@gmail.com>
2025-09-10 21:46:45 +0530

    Delete repo directory

--- repo/static/js/main.js
+++
@@ -1,6 +0,0 @@
-const sendBtn = document.getElementById('sendBtn');
-const formStatus = document.getElementById('formStatus');
-
-sendBtn.addEventListener('click', () => {
-  formStatus.innerText = "This is a static site — form not connected.";
-});
